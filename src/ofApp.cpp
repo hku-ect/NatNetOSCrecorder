e6bfcee97ebe819355c1d88314450c65194649a8
Arnaud Loonstra <arnaud@sphaero.org>
2019-04-17 13:12:46 +0200

    added red color to record button when recording

--- src/ofApp.cpp
+++ src/ofApp.cpp
@@ -459,6 +459,14 @@ void ofApp::doGui() {
                 
         // Toggle OSC recording
         // FIXME: make a proper toggle button
+        bool wasRecording = isRecording;
+        if (wasRecording)
+        {
+            ImGui::PushStyleColor(ImGuiCol_Button, (ImVec4)ImColor::HSV(1.0f, 0.6f, 0.6f));
+            ImGui::PushStyleColor(ImGuiCol_ButtonHovered, (ImVec4)ImColor::HSV(1.0f, 0.7f, 0.7f));
+            ImGui::PushStyleColor(ImGuiCol_ButtonActive, (ImVec4)ImColor::HSV(1.0f, 0.8f, 0.8f));
+        }
+
         if ( ImGui::Button(ICON_FA_CIRCLE " Record OSC") )
         {
             if ( ! isRecording )
@@ -473,7 +481,8 @@ void ofApp::doGui() {
                 isRecording = false;
             }
         }
-        
+        if (wasRecording) ImGui::PopStyleColor(3);
+
         ImGui::Spacing();
         ImGui::Separator();
         ImGui::Spacing();
